307b7a90b542beaeabc490cd7e58bf5363d72a54
daniel687 <anthony.torretti@gmail.com>
2017-04-26 18:52:32 +0200

    Migrations Added and Models

---
+++ database/migrations/2017_04_26_300000_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('jobdate');
+            $table->string('amount');
+
+            $table->integer('vehicleid')->unsigned();
+        });
+
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->foreign('vehicleid')->references('id')->on('vehicles')->onDelete('cascade');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
