6b348c9f887f92ec9905a3f0f625fe53de5b7957
federicoangeloni <federico.angeloni@yahoo.it>
2017-05-21 23:20:32 +0200

    Modificata pagina Welcome iniziale in modo da avere tutte le parti fatte e collegate nella home per praticità

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,86 +10,25 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
+
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+    <div>
+        <h1>Gestione Clienti</h1>
+        <a href="{{ url('/customers') }}">Elenca tutti i clienti</a><br>
+        <a href="{{ url('/customers/search') }}">Cerca un cliente (filtri)</a><br>
+        <a href="{{ url('/customers/add') }}">Aggiungi un nuovo cliente</a><br>
+        <a href="{{ url('/customers/1') }}">Visualizza cliente (per id)</a><br>
+        <hr>
+        <h1>Gestione Veicoli</h1>
+        <a href="{{ url('/vehicles') }}">Elenca tutti i veicoli</a><br>
+        <a href="{{ url('/vehicles/search') }}">Cerca un veicolo (filtri)</a><br>
+        <a href="{{ url('/vehicles/search/1') }}">Visualizza veicoli di un cliente(id cliente)</a><br>
+        <a href="{{ url('/vehicles/1') }}">Visualizza veicolo (id veicolo)</a><br>
+        <a href="{{ url('/vehicles/add/1') }}">Aggiungi un nuovo veicolo (relazionato con un cliente)</a><br>
+        <hr>
+    </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
     </body>
 </html>
